eecf42240afaee2f3f3684b29c05ea4e61924738
steveoro <steve.alloro@gmail.com>
2015-08-27 20:31:06 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,4 @@
+## Goggles Usage stats reporter  -o^o-
+
+Simple app to show collected usage stats for master-goggles.org.
+Developed for internal-usage only.
---
+++ config/initializers/redis.rb
@@ -0,0 +1,5 @@
+# Create a new local client instance for Redis:
+$redis = Redis::Namespace.new(
+  "goggles_stats",
+  redis: Redis.new(host: 'localhost', port: 6379)
+)
